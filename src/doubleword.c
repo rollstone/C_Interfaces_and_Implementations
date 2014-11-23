c3f04c9320c0ba83ccb581e54a5a6d7cbcc8b0e1
rollstone <ggyanlei@gmail.com>
2014-11-23 10:56:13 +0800

    Finish reading Chapter 1, exercise 3 needs to be take care.

---
+++ src/doubleword.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctypes.h>
+#include <string.h>
+
+int linenum;
+
+int getword(FILE *, char *, int);
+
+void doubleword(char *, FILE *);
+
+int main(int argc, const char *argv[])
+{
+    int i;
+
+    for (i = 1; i < argc; i++) {
+        FILE *fp = fopen("argv[i]", "r");
+        if (fp == NULL) {
+            fprintf(stderr, "%s: can't open '%s' (%s)\n", argv[0], argv[i], strerror(errno));
+            return EXIT_FAILURE;
+        } else {
+            doubleword(argv[i], fp);
+            fclose(fp);
+        }
+    }
+
+    if (argc == 1) {
+        doubleword(NULL, stdin);
+    }
+    return EXIT_SUCCESS;
+}
+
+int getword(FILE *fp, char *buf, int size)
+{
+    int c;
+
+    c = getc(fp);
+    /*scan forward to a nonspace char or EOF 6*/
+    for ( ; c != EOF && isspace(c); c = getc(fp)) {
+        if (c == '\n') {
+            linenum++;
+        }
+    }
+
+    {
+        int i = 0;
+        for ( ; c != EOF && !isspace(fp); c = getc(fp)) {
+            if (i < size - 1) {
+                buf[i++] = tolower(c);
+            }
+        }
+
+        if (i < size) {
+            buf[i] = '\0';
+        }
+    }
+
+    /*copy the word into buf[0..size-1] 7*/
+    if (c != EOF) {
+        ungetc(c, fp);
+    }
+
+    return (buf[0] != '\0');
+}
+
+void doubleword(char *name, FILE *fp)
+{
+    char prev[128], word[128];
+
+    linenum = 1;
+    prev[0] = '\0';
+
+    while (getword(fp, word, sizeof(word))) {
+        if (isalpha(word[0]) && strcmp(prev, word) == 0) {
+            /*word is a duplicate 8*/
+            {
+                if (name) {
+                    printf("%s:\n", name);
+                }
+                printf("%d: %s\n", linenum, word);
+            }
+        }
+        strcpy(prev, word);
+    }
+}
